09ebc6de0f5aae47739863baadcc276ef476e22e
ii2day <ji.li@daocloud.io>
2023-07-03 15:14:24 +0800

    remove e2e example case
    Signed-off-by: ii2day <ji.li@daocloud.io>

--- test/e2e/example/example_test.go
+++ test/e2e/example/example_test.go
@@ -4,7 +4,6 @@ package example_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("example ", Label("example"), func() {
@@ -12,8 +11,5 @@ var _ = Describe("example ", Label("example"), func() {
 	It("example", Label("example-1"), func() {
 		GinkgoWriter.Printf("cluster information: %+v \n", frame.Info)
 
-		d, e := frame.GetDaemonSet("test", "default")
-		Expect(e).NotTo(HaveOccurred(), "failed to get daemonset ")
-		GinkgoWriter.Printf("daemonset: %+v \n", d)
 	})
 })
