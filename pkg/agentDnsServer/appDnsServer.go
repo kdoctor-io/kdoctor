0411e27717d891e44d9d581ae0107e961fe87c8d
ii2day <ji.li@daocloud.io>
2023-12-11 14:01:23 +0800

    Enhance agent robustness in app mode && chart value optimization
    Signed-off-by: ii2day <ji.li@daocloud.io>

--- pkg/agentDnsServer/appDnsServer.go
+++ pkg/agentDnsServer/appDnsServer.go
@@ -7,25 +7,33 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"net"
+	"strings"
+
+	"github.com/miekg/dns"
+	"go.uber.org/zap"
+
 	"github.com/kdoctor-io/kdoctor/pkg/agentHttpServer"
 	k8sObjManager "github.com/kdoctor-io/kdoctor/pkg/k8ObjManager"
 	"github.com/kdoctor-io/kdoctor/pkg/types"
-	"github.com/miekg/dns"
-	"go.uber.org/zap"
-	"net"
-	"strings"
 )
 
 func SetupAppDnsServer(rootLogger *zap.Logger, tlsCert, tlsKey string) {
 	logger := rootLogger.Named("app dns server")
 	logger.Sugar().Infof("setup app dns Server")
-	cert, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
-	if err != nil {
-		logger.Sugar().Fatalf("failed load cert key from path %s %s", tlsCert, tlsKey)
+	var tlsConfig *tls.Config
+	var err error
+	if tlsCert != "" && tlsKey != "" {
+		cert, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
+		if err != nil {
+			logger.Sugar().Errorf("failed load cert key from path %s %s", tlsCert, tlsKey)
+		} else {
+			tlsConfig = &tls.Config{
+				Certificates: []tls.Certificate{cert},
+			}
+		}
 	}
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-	}
+
 	var resolver *dns.Client
 	var coreDnsAddr string
 	if types.AgentConfig.AppDnsUpstream {
@@ -34,16 +42,18 @@ func SetupAppDnsServer(rootLogger *zap.Logger, tlsCert, tlsKey string) {
 		}
 		dnsServiceIPs, err := k8sObjManager.GetK8sObjManager().ListServicesDnsIP(context.Background())
 		if err != nil {
-			logger.Sugar().Fatalf("failed get kube dns service,err: %v", err)
+			logger.Sugar().Errorf("failed get kube dns service,err: %v", err)
 		}
 
 		// If the ip address list is empty, the corresponding service does not exist or the selected label is incorrect
 		if len(dnsServiceIPs) == 0 {
-			logger.Sugar().Fatalf("failed get kube dns service: %v", "the corresponding service does not exist or the selected label is incorrect")
+			logger.Sugar().Errorf("failed get kube dns service: %v", "the corresponding service does not exist or the selected label is incorrect")
+			coreDnsAddr = ""
+		} else {
+			logger.Sugar().Infof("kube dns service %s ", dnsServiceIPs)
+			coreDnsAddr = fmt.Sprintf("%s:53", dnsServiceIPs[0])
 		}
 
-		logger.Sugar().Infof("kube dns service %s ", dnsServiceIPs)
-		coreDnsAddr = fmt.Sprintf("%s:53", dnsServiceIPs[0])
 	}
 	handler := dns.HandlerFunc(func(w dns.ResponseWriter, r *dns.Msg) {
 		qname := r.Question[0].Name
@@ -63,7 +73,7 @@ func SetupAppDnsServer(rootLogger *zap.Logger, tlsCert, tlsKey string) {
 			}
 			task := strings.Split(qname, ".")[0]
 			agentHttpServer.RequestCounts.AddOneCount(task)
-		} else if types.AgentConfig.AppDnsUpstream {
+		} else if types.AgentConfig.AppDnsUpstream && coreDnsAddr != "" {
 			m, _, err = resolver.Exchange(r, coreDnsAddr)
 			if err != nil {
 				fmt.Println("Error forwarding DNS query:", err)
@@ -74,17 +84,33 @@ func SetupAppDnsServer(rootLogger *zap.Logger, tlsCert, tlsKey string) {
 
 	})
 
-	tlsServer := &dns.Server{
-		Addr:      fmt.Sprintf(":%d", types.AgentConfig.AppDnsTcpTlsPort),
-		Net:       "tcp-tls",
-		TLSConfig: tlsConfig,
-		Handler:   handler,
+	if tlsConfig != nil {
+		tlsServer := &dns.Server{
+			Addr:      fmt.Sprintf(":%d", types.AgentConfig.AppDnsTcpTlsPort),
+			Net:       "tcp-tls",
+			TLSConfig: tlsConfig,
+			Handler:   handler,
+		}
+		go func() {
+			logger.Sugar().Infof("dns tcp-tls server listien %s", tlsServer.Addr)
+			if err := tlsServer.ListenAndServe(); err != nil {
+				logger.Sugar().Errorf("dns tcp-tls server , err: %v ", err)
+			}
+		}()
 	}
+
 	udpServer := &dns.Server{
 		Addr:    fmt.Sprintf(":%d", types.AgentConfig.AppDnsUdpPort),
 		Net:     "udp",
 		Handler: handler,
 	}
+
+	go func() {
+		logger.Sugar().Infof("dns udp server listien %s", udpServer.Addr)
+		if err := udpServer.ListenAndServe(); err != nil {
+			logger.Sugar().Errorf("dns udp server , err: %v ", err)
+		}
+	}()
 
 	tcpServer := &dns.Server{
 		Addr:    fmt.Sprintf(":%d", types.AgentConfig.AppDnsTcpPort),
@@ -94,21 +120,8 @@ func SetupAppDnsServer(rootLogger *zap.Logger, tlsCert, tlsKey string) {
 	go func() {
 		logger.Sugar().Infof("dns tcp server listien %s", tcpServer.Addr)
 		if err := tcpServer.ListenAndServe(); err != nil {
-			logger.Sugar().Fatalf("dns tcp server , err: %v ", err)
+			logger.Sugar().Errorf("dns tcp server , err: %v ", err)
 		}
 	}()
 
-	go func() {
-		logger.Sugar().Infof("dns udp server listien %s", udpServer.Addr)
-		if err := udpServer.ListenAndServe(); err != nil {
-			logger.Sugar().Fatalf("dns udp server , err: %v ", err)
-		}
-	}()
-
-	go func() {
-		logger.Sugar().Infof("dns tcp-tls server listien %s", tlsServer.Addr)
-		if err := tlsServer.ListenAndServe(); err != nil {
-			logger.Sugar().Fatalf("dns tcp-tls server , err: %v ", err)
-		}
-	}()
 }
